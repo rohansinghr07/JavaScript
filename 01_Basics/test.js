8e9306e790c72fe336c51cf960e1669f8ac4e1e2
rohansinghr07 <156138600+rohansinghr07@users.noreply.github.com>
2024-04-24 21:59:20 +0000

    datatypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,13 @@
-console.log("Rohan")
+const accountId = 12345
+let accountEmail = "rohansingh161103@gmail.com"
+var accountPassword = "1234"
+accountCity = "Prayagraj"
+
+//accountId = 7 => not allowed
+accountEmail = "rohansinghr07@gmail.com"
+accountPassword = "121212"
+accountCity = "Allahabad"
+
+
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountCity])
